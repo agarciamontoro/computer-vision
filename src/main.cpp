e607a0df75c2bcdb29f1a5d0b62ca57f17d3d68d
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-10-28 12:53:45 +0100

    Final main structure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,38 @@
 #include "image.hpp"
 
 int main(){
-     Image bike("imagenes/bicycle.bmp");
-     Image motorbike("imagenes/motorcycle.bmp",false);
+    Image bird("imagenes/bird.bmp");
+    Image blurred_bird = bird.lowPassFilter(3.0);
 
-     Image hybrid_canvas = makeHybridCanvas(motorbike,bike,4.0,3.0);
+    bird.setName("Bird image.");
+    blurred_bird.setName("Low-pass filtered bird image.");
 
-     hybrid_canvas.setName("Hybrid canvas.");
-     hybrid_canvas.draw();
+    bird.draw();
+    blurred_bird.draw();
 
-     Image fish("imagenes/fish.bmp");
-     Image submarine("imagenes/submarine.bmp");
+    waitKey(0);
+    destroyAllWindows();
 
-     Image hybrid = submarine.hybrid(fish,4.0,3.0);
-     Image pyramid_canvas = hybrid.makePyramidCanvas(5);
+    Image bike("imagenes/bicycle.bmp");
+    Image motorbike("imagenes/motorcycle.bmp",false);
 
-     pyramid_canvas.setName("Gaussian pyramid.");
-     pyramid_canvas.draw();
+    Image hybrid_canvas = makeHybridCanvas(motorbike,bike,4.0,3.0);
 
-     waitKey(0);
-     destroyAllWindows();
+    hybrid_canvas.setName("Hybrid canvas.");
+    hybrid_canvas.draw();
+
+    waitKey(0);
+    destroyAllWindows();
+
+    Image fish("imagenes/fish.bmp");
+    Image submarine("imagenes/submarine.bmp");
+
+    Image hybrid = submarine.hybrid(fish,4.0,3.0);
+    Image pyramid_canvas = hybrid.makePyramidCanvas(5);
+
+    pyramid_canvas.setName("Gaussian pyramid.");
+    pyramid_canvas.draw();
+
+    waitKey(0);
+    destroyAllWindows();
 }
