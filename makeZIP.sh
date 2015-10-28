cac422a863bc0566d21e59e0b68c9c5aa6802e10
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-10-28 20:08:09 +0100

    Adds ugly, simple, inefficient, script to make the ZIP easier

---
+++ makeZIP.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to make the process of zipping the work automatic.
+# Arguments: number of the assignment with two digits
+
+FOLDER=../GarciaMontoro_Alejandro-$1
+
+mkdir $FOLDER
+
+echo "Selecting the right files for you..."
+
+cp -r inc $FOLDER/inc
+cp -r src $FOLDER/src
+cp -r Informes/I_$1.pdf $FOLDER/Informe_$1.pdf
+
+echo "Zipping everything..."
+
+zip -r $FOLDER.zip $FOLDER
+
+rm -r $FOLDER
+
+echo "Done! Do not forget to send the zip to the teacher :)"
